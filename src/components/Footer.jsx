41050af4b62d11231066d28e66dd30a069d41a86
Muhammed Zain <muhammedzain2256@gmail.com>
2023-11-21 22:06:04 +0500

    Footer Section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="flex justify-between p-4 bg-gray-700 ">
+      <div className='border-2 border-gray-600 p-4 bg-white text-black rounded-md'>
+        <div className="bg-white text-black">Help/FAQ</div>
+      </div>
+      <div>
+        <p className="border-2 border-gray-600 bg-white text-black p-4 rounded-md">© E-Commerce React</p>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
