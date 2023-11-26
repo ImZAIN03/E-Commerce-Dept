6fa3ed701a631b28ecfd3d0a415b71166898a713
Muhammed Zain <muhammedzain2256@gmail.com>
2023-11-26 23:49:38 +0500

    Cart Changes

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,4 +1,3 @@
-// Cart.jsx
 import React from 'react';
 
 const Cart = ({ cartItems, removeFromCart }) => {
@@ -8,36 +7,45 @@ const Cart = ({ cartItems, removeFromCart }) => {
     removeFromCart(productId);
   };
 
+  const totalPrice = cartItems.reduce((total, cartItem) => {
+    return total + cartItem.price * cartItem.quantity;
+  }, 0);
+
   return (
-    <div className="container mx-auto p-4">
+    <div className="container mx-auto p-4 w-4/6 ">
       <h2 className="text-3xl font-semibold mb-4">Shopping Cart</h2>
       {cartItems && cartItems.length === 0 ? (
         <p>Your cart is empty.</p>
       ) : (
-        <ul>
-          {cartItems?.map((cartItem) => (
-            <li key={cartItem.id} className="mb-2">
-              <div className="flex items-center">
-                <img
-                  src={cartItem.image}
-                  alt={cartItem.name}
-                  className="w-16 h-16 object-cover mr-4"
-                />
-                <div>
-                  <p className="text-lg font-semibold">{cartItem.name}</p>
-                  <p className="text-gray-600">{cartItem.description}</p>
-                  <p className="text-gray-800">${cartItem.price} x {cartItem.quantity}</p>
-                  <button
-                    onClick={() => handleRemove(cartItem.id)}
-                    className="text-red-500 font-semibold hover:underline focus:outline-none"
-                  >
-                    Remove
-                  </button>
+        <div>
+          <ul>
+            {cartItems?.map((cartItem) => (
+              <li key={cartItem.id} className="mb-2">
+                <div className="flex items-center">
+                  <img
+                    src={cartItem.image}
+                    alt={cartItem.name}
+                    className="w-16 h-16 object-cover mr-4"
+                  />
+                  <div>
+                    <p className="text-lg font-semibold">{cartItem.name}</p>
+                    <p className="text-gray-600">{cartItem.description}</p>
+                    <p className="text-gray-800">${cartItem.price} x {cartItem.quantity}</p>
+                    <button
+                      onClick={() => handleRemove(cartItem.id)}
+                      className="text-red-500 font-semibold hover:underline focus:outline-none"
+                    >
+                      Remove
+                    </button>
+                  </div>
                 </div>
-              </div>
-            </li>
-          ))}
-        </ul>
+              </li>
+            ))}
+          </ul>
+          <div className="mt-4">
+            <p className="text-lg font-semibold">Total: ${totalPrice.toFixed(2)}</p>
+          </div>
+        </div>
       )}
     </div>
   );
